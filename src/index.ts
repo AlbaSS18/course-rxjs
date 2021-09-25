a6922ccf90f72f48e803a9a1c5a31ffbb2ce86dc
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-09-25 18:59:52 +0200

    subscribe method

--- src/index.ts
+++ src/index.ts
@@ -3,13 +3,23 @@ import { Observable } from "rxjs";
 //const obs$ = Observable.create(); Another way. Not recommended.
 
 const obs$ = new Observable<string>(subs => {
-    
+
     subs.next('Hola'); // Next emite el valor entre paréntesis.
 
     subs.complete(); // Ninguna emisión posterior va a ser notificada a sus subscriptores.
 });
 
-obs$.subscribe(console.log); // Subscripciones
+/* obs$.subscribe({
+    next: (value) => console.log('next', value),
+    error: (error) => console.warn('error', error),
+    complete: () => console.info('complete') 
+}); // Subscripciones */
+
+obs$.subscribe(
+    value => console.log('next', value),
+    error => console.warn('error', error),
+    () => console.info('complete') 
+); // Subscripciones
 
 
 
