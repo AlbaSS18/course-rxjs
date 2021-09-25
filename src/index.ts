8ad75e9b79c8e35a4e289d8b5417de6382d27955
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-09-25 18:30:39 +0200

    first observable

--- src/index.ts
+++ src/index.ts
@@ -1,8 +1,15 @@
+import { Observable } from "rxjs";
 
+//const obs$ = Observable.create(); Another way. Not recommended.
 
-console.log('Hola Mundo!');
+const obs$ = new Observable<string>(subs => {
+    
+    subs.next('Hola'); // Next emite el valor entre paréntesis.
 
+    subs.complete(); // Ninguna emisión posterior va a ser notificada a sus subscriptores.
+});
 
+obs$.subscribe(console.log); // Subscripciones
 
 
 
