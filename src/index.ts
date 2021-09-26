82b252935b45a088a9d4a92a96e8453f04aeeeef
AlbaSS18 <albaserenasuarez1998@gmail.com>
2021-09-26 14:45:25 +0200

    remove commented code

--- src/index.ts
+++ src/index.ts
@@ -1,27 +1,15 @@
-import { Observable } from "rxjs";
+import { Observable } from 'rxjs'
 
-//const obs$ = Observable.create(); Another way. Not recommended.
+// const obs$ = Observable.create(); Another way. Not recommended.
 
 const obs$ = new Observable<string>(subs => {
+  subs.next('Hola') // Next emite el valor entre paréntesis.
 
-    subs.next('Hola'); // Next emite el valor entre paréntesis.
+  subs.complete() // Ninguna emisión posterior va a ser notificada a sus subscriptores.
+})
 
-    subs.complete(); // Ninguna emisión posterior va a ser notificada a sus subscriptores.
-});
-
-/* obs$.subscribe({
-    next: (value) => console.log('next', value),
-    error: (error) => console.warn('error', error),
-    complete: () => console.info('complete') 
-}); // Subscripciones */
-
-obs$.subscribe(
-    value => console.log('next', value),
-    error => console.warn('error', error),
-    () => console.info('complete') 
-); // Subscripciones
-
-
-
-
-
+obs$.subscribe({
+  next: (value) => console.log('next', value),
+  error: (error) => console.warn('error', error),
+  complete: () => console.info('complete')
+}); // Subscripciones
